defe9f6849a7695d0788751432997ac8186437ae
Anna <andranna567@gmail.com>
2020-08-13 22:51:22 +0700

    Class "news_screen" created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'news_card.dart';
 import 'news_screen.dart';
 
 void main() => runApp(new ItWayApp());
@@ -8,23 +7,6 @@ void main() => runApp(new ItWayApp());
 class ItWayApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('News IT Way'),
-        ),
-        body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            NewsCard(
-              newsTitle: 'News 1',
-              newsSubtitle: 'Secondary text',
-              newsImage: 'images/news1.jpg',
-            ),
-          ],
-        )),
-      ),
-    );
+    return NewsScreen();
   }
 }
--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -1,1 +1,44 @@
 import 'package:flutter/material.dart';
+import 'news_data.dart';
+import 'news_card.dart';
+
+class NewsScreen extends StatefulWidget {
+  @override
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  List<NewsCard> newsData = [
+    NewsCard(
+        newsTitle: 'News 1',
+        newsSubtitle: 'Secondary text',
+        newsImage: 'images/news1.jpg'),
+    NewsCard(
+        newsTitle: 'News 2',
+        newsSubtitle: 'Secondary text',
+        newsImage: 'images/news1.jpg'),
+    NewsCard(
+        newsTitle: 'News 2',
+        newsSubtitle: 'Secondary text',
+        newsImage: 'images/news1.jpg')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('News IT Way'),
+        ),
+        body: SafeArea(
+          child: ListView.builder(
+              itemCount: newsData.length,
+              itemBuilder: (context, index) {
+                return newsData[index];
+              }),
+        ),
+      ),
+    );
+  }
+}
